2d455bf58d53e8ab5625fa1ccdaddae2bb5908fc
DarChris10 <darchris1007@gmail.com>
2024-06-12 23:40:13 -0400

    UI Layout

--- composeApp/src/androidMain/kotlin/org/multicalculator/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/multicalculator/project/MainActivity.kt
@@ -3,6 +3,13 @@ package org.multicalculator.project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +18,28 @@ class MainActivity : ComponentActivity() {
             CalcView()
         }
     }
+}
+
+@Composable
+fun CalcView() {
+    val displayText = remember { mutableStateOf("0") }
+
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        CalcDisplay(display = displayText)
+    }
+}
+
+@Composable
+fun CalcDisplay(display: MutableState<String>) {
+    Text(
+        text = display.value,
+        modifier = Modifier
+            .background(Color.White)
+            .padding(16.dp)
+    )
 }
